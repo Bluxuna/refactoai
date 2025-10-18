15f3f0c1c512925a7c9beaeab13a26bc200453dc
kume1a <kumela011@gmail.com>
2025-10-18 15:09:25 +0400

    add .gitignore to exclude __pycache__ and tasks.db; refactor create_db.py to set project root path

--- server/scripts/create_db.py
+++ server/scripts/create_db.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+project_root = Path(__file__).parent.parent.parent
+sys.path.insert(0, str(project_root))
+
 from server.database.db_models import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
